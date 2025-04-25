24a750334994587e1bdb6e67559a0f40276d3126
Romain Centurioni <romain.centurioni@oclock.school>
2025-04-25 14:10:07 +0200

    Associations of Sequelize Models

---
+++ src/models/associations.js
@@ -0,0 +1,59 @@
+import { Game } from "./game.model.js"
+import { Challenge } from "./challenge.model.js"
+import { Realization } from "./realization.model.js"
+import { Category } from "./category.model.js"
+import { User } from "./user.model.js"
+import { Platform } from "./platform.model.js"
+
+Game.hasMany(Challenge, {
+    onDelete: 'CASCADE'
+}); 
+Challenge.belongsTo(Game);
+
+Game.belongsToMany(Platform, {
+    through: 'GamePlatform'
+});
+Platform.belongsToMany(Game, {
+    through: 'GamePlatform'
+});
+
+Category.hasMany(Challenge, {
+    onDelete:'CASACADE'
+});
+Challenge.belongsTo(Category);
+
+Challenge.hasMany(Realization, {
+    onDelete:'CASCADE'
+});
+Realization.belongsTo(Challenge);
+
+User.hasMany(Challenge, {
+    onDelete: 'CASCADE'
+});
+Challenge.belongsTo(User);
+
+User.belongsToMany(Challenge, {
+    through: 'UserLikeChallenge'
+});
+Challenge.belongsToMany(User, {
+    through: 'UserLikeChallenge'
+}); 
+
+User.hasMany(Challenge, {
+    onDelete: 'CASCADE'
+});
+Challenge.belongsTo(User);
+
+User.hasMany(Realization, {
+    onDelete: 'CASCADE'
+});
+Realization.belongsTo(User);
+
+User.belongsToMany(Realization, {
+    through: 'UserRealization'
+});
+Realization.belongsToMany(User, {
+    through: 'UserRealization'
+});
+
+export { Game, Platform, Challenge, User, Realization, Category };

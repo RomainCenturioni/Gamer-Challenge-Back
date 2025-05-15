2ec50959012cb3c18d3da738c3dc4aab9ec851b3
Hugo Castejon <hugocastejon7@gmail.com>
2025-05-15 14:31:07 +0200

    commit au propre

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
+<<<<<<< HEAD
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
@@ -14,8 +15,11 @@ app.use(
       'http://127.0.0.1:5174',
       'http://localhost:8443'
     ],
+=======
+    origin: ["http://localhost:5173", "http://localhost:8443", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+>>>>>>> commit au propre
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());

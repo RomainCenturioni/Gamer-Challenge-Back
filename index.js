37fc79bd32215ad05940512e8da472b25bb10310
Romain Centurioni <romain.centurioni@oclock.school>
2025-05-16 11:51:49 +0200

    index.js modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://172.29.145.22:8443/'],
+    origin: 'http://172.29.145.22:8443/',
     credentials: true,
   }),
 );

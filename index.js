513d31bfa3ccb60a19d8b55330c7b80d73da7c22
Romain Centurioni <romain.centurioni@oclock.school>
2025-05-16 10:35:13 +0200

    cleaning index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-<<<<<<< HEAD
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
@@ -15,9 +14,6 @@ app.use(
       'http://127.0.0.1:5174',
       'http://localhost:8443'
     ],
-=======
-    origin: ["http://localhost:5173", "http://localhost:8443", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
->>>>>>> commit au propre
     credentials: true,
   })
 );

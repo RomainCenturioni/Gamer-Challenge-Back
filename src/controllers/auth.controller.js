d2f26396e15c51c90390898176b9c71aa8f0fae2
Romain Centurioni <romain.centurioni@oclock.school>
2025-05-16 15:01:40 +0200

    cleaning for prod

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,7 +49,7 @@ export const authController = {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "lax",
         maxAge: 2 * 60 * 60 * 1000,
       });
 

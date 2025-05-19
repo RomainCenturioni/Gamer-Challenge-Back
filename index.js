099fa6af7bc6c958b6af802a6ee169f2999a5be3
Romain Centurioni <romain.centurioni@oclock.school>
2025-05-19 11:28:21 +0200

    test prod

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { router } from './src/routers/index.router.js';
 import cookieParser from 'cookie-parser';
+import { configDotenv } from 'dotenv';
+configDotenv();
 
 const app = express();
 app.use(cookieParser());
